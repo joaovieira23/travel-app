ee0cd80230c5996a14a5aecb3e2ea132693cedfc
Joao Andrade <joaovictorvieira.23@hotmail.com>
2022-05-24 20:13:54 -0500

    feat: tabBarController with TickerProviderState

--- lib/pages/navpages/home_page.dart
+++ lib/pages/navpages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:travel_app/widgets/app_large_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,9 +10,65 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return Container();
-  }
-}
+      TabController _tabController = TabController(length: 3, vsync: this);
+      return Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //menu text
+            Container(
+              padding: const EdgeInsets.only(top: 70, left: 20),
+              child: Row(
+                children: [
+                  Icon(Icons.menu, size: 30, color: Colors.black54),
+                  Expanded(child: Container()),
+                  Container(
+                    margin: const EdgeInsets.only(right: 20),
+                    width: 50,
+                    height: 50,
+
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey.withOpacity(0.5),
+                    ),
+                  ),
+                ]
+              ),
+            ),
+            SizedBox(height: 40,),
+            //discover text
+            Container(
+              margin: const EdgeInsets.only(left: 20),
+              child: AppLargeText(text: "Discover"),
+            ),
+            SizedBox(height: 30,),
+            //TabBar
+            Container(
+              child: TabBar(
+                controller: _tabController,
+                tabs: [
+                  Tab(text: 'Places'),
+                  Tab(text: 'Inspiration'),
+                  Tab(text: 'Emotions'),
+                ]
+              ),
+            ),
+            Container(
+              height: 300,
+              width: double.maxFinite,
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  Text("Hi"),
+                  Text("There"),
+                  Text("Bye"),
+                ]),
+              )
+            ],
+          ),
+        );
+      }
+    }
